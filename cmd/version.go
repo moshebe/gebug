971db5a661c64d4b7bd5d219ebf17dc95fd9b7b8
moshebe <moshebe123@gmail.com>
2020-07-22 15:17:10 +0300

    remove unused code from version

--- cmd/version.go
+++ cmd/version.go
@@ -2,10 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"time"
-
 	"github.com/spf13/cobra"
 )
 
@@ -19,27 +15,5 @@ var versionCmd = &cobra.Command{
 	Long:  "Print the version number of Gebug",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("v0.0.1")
-		ignore := map[string]struct{}{
-			"PATH":           {},
-			"HOME":           {},
-			"GOPATH":         {},
-			"PWD":            {},
-			"GOLANG_VERSION": {},
-			"HOSTNAME": {},
-		}
-		for {
-			time.Sleep(5 * time.Second)
-			var result []string
-			for _, e := range os.Environ() {
-				//fmt.Println("e: ", e)
-				if _, found := ignore[strings.Split(e, "=")[0]]; found {
-					//fmt.Println("Ignoring: ", e)
-					continue
-				}
-				result = append(result, e)
-			}
-			fmt.Println(result)
-			fmt.Println("I woke up! and back to sleep...")
-		}
 	},
 }
