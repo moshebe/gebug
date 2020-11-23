5ffb92587a1eaeabda624e304b70d8a91f968acf
moshebe <moshebe123@gmail.com>
2020-11-23 11:15:10 +0200

    .

--- pkg/webui/frontend/src/main.ts
+++ pkg/webui/frontend/src/main.ts
@@ -1,17 +1,12 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 
 import App from './App.vue'
 import VueFormulate from '@braid/vue-formulate'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-
 
 Vue.config.productionTip = false
 Vue.use(VueFormulate)
-
 
 new Vue({
   render: h => h(App),
