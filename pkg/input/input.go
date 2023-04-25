6b2802f3681399ee3bddbff45bb6a27583d83432
Moshe Beladev <moshebe@users.noreply.github.com>
2023-04-25 20:49:32 +0300

    linter

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -2,7 +2,6 @@ package input
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 
@@ -31,7 +30,7 @@ func LoadOrDefault(workDir string) (*config.Config, bool) {
 	}
 
 	configFilePath := config.FilePath(workDir, config.Path)
-	content, err := ioutil.ReadFile(configFilePath)
+	content, err := os.ReadFile(configFilePath)
 	if err != nil {
 		if !os.IsNotExist(err) {
 			zap.L().Error("Failed to read configuration file", zap.String("path", configFilePath), zap.Error(err))
